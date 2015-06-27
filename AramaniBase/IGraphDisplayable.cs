8b313bfc7fd7af9fe7dfdb6781b3cdba41a2372d
Pachelbel <gaiseras@gmail.com>
2015-06-27 20:19:22 +0200

    g

---
+++ AramaniBase/IGraphDisplayable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// TODO 
+
+namespace Aramani
+{
+    /// <summary>
+    /// Implemented by entities containing to a graph structure, 
+    /// for displaying them as simple text or in dot format.
+    /// </summary>
+    public interface IGraphDisplayable
+    {
+
+        /// <summary>
+        /// Return code fragment in dot format
+        /// describing the object.
+        /// </summary>
+        /// <returns></returns>
+        string AsDot();
+
+        /// <summary>
+        /// Return a simple textual description of
+        /// the object.
+        /// </summary>
+        /// <returns></returns>
+        string Description { get; }
+    }
+}
