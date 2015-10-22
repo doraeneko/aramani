e0bd8093b00a5d0c39da517b84081d5b98958e66
Pachelbel <gaiseras@gmail.com>
2015-10-22 13:53:00 +0200

    add basicblocks

---
+++ AramaniIR/BasicBlocks/BasicBlock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Aramani.IR.Commands;
+using Aramani.Base;
+
+namespace Aramani.IR.BasicBlocks
+{
+    public class  BasicBlock : IDescription
+    {
+        public BasicBlock()
+        {
+            Code = new List<Command>();
+        }
+
+        public ICollection<Command> Code { get; set; }
+
+        public string Description
+        {
+            get 
+            {
+                var result = "";
+                foreach (var command in Code)
+                {
+                    result += command.Description;
+                }
+                return result;
+            }
+        }
+    }
+}
